31bc1b92e05a6745f0020af1cea0b90546645eea
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-03-25 17:09:21 -0300

    Refactor ship movements

--- game_functions.py
+++ game_functions.py
@@ -4,7 +4,7 @@ import pygame
 
 
 def check_events(ship):
-    """Respond to keypresses and mouse events"""
+    """Respond to mouse events"""
 
     for event in pygame.event.get():
         # If the user closes the window, quits the program
@@ -12,16 +12,25 @@ def check_events(ship):
             sys.exit()
 
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = True
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = True
+            check_keydown_events(event, ship)
 
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                ship.moving_right = False
-            elif event.key == pygame.K_LEFT:
-                ship.moving_left = False
+            check_keyup_events(event, ship)
+            
+
+def check_keydown_events(event, ship):
+    """Respond to keypresses"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = True
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = True
+
+def check_keyup_events(event, ship):
+    """Respond to keyreleases"""
+    if event.key == pygame.K_RIGHT:
+        ship.moving_right = False
+    elif event.key == pygame.K_LEFT:
+        ship.moving_left = False
 
 
 def update_screen(ai_settings, screen, ship):
