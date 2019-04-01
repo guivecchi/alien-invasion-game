81bc42477c487c50b159868029d063b5155a93b3
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-04-01 17:36:03 -0300

    Detects when ship hits the bottom of the screen
    Detects if ship hits the bottom of the screen and
    behaves the same way as if it was a collision between
    the ship and an alien

--- game_functions.py
+++ game_functions.py
@@ -131,6 +131,9 @@ def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     if pygame.sprite.spritecollideany(ship, aliens):
         ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
 
+    # Look for aliens hitting the bottom of the screen.
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
+
 def check_fleet_edges(ai_settings, aliens):
     """Respond appropriately if any aliens have reached an edge."""
     for alien in aliens.sprites():
@@ -159,3 +162,12 @@ def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
     
     # Pause.
     sleep(0.5)
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """Check if any aliens have reached the bottom of the screen."""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            # Treat this the same as if the ship got hit.
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
