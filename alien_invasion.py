017b84b5a5bbe98b4542d4194b31d7dc5a6f5a6e
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-04-01 14:41:14 -0300

    Add bullet class
    Add bullet class and update alien_invasion, game_functions and settings for the class to work

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,6 +2,8 @@ import pygame
 
 from settings import Settings
 from ship import Ship
+from pygame.sprite import Group
+
 import game_functions as gf
 
 
@@ -16,11 +18,15 @@ def run_game():
     # Make a ship.
     ship = Ship(ai_settings, screen)
 
+    # Make a group to store bullets in.
+    bullets = Group()
+
     # Start the main loop for the game.
     while True:
-        gf.check_events(ship)
+        gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
-        gf.update_screen(ai_settings, screen, ship)
+        bullets.update()
+        gf.update_screen(ai_settings, screen, ship, bullets)
 
 
 run_game()
--- settings.py
+++ settings.py
@@ -10,3 +10,9 @@ class Settings():
 
         # Ship setting
         self.ship_speed_factor = 1.5
+
+        # Bullet settings
+        self.bullet_speed_factor = 1
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 60, 60, 60
