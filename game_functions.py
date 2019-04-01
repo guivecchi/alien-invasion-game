70d32d34fbad4801a6bb2ea0476ab9794530d19f
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-04-01 14:51:54 -0300

    Update bullets functions to delete and limit the bullets to 3

--- game_functions.py
+++ game_functions.py
@@ -26,10 +26,7 @@ def check_keydown_events(event, ai_settings, screen, ship, bullets):
     elif event.key == pygame.K_LEFT:
         ship.moving_left = True
     elif event.key == pygame.K_SPACE:
-        # Create a new bullet and add it to the bullets group.
-        new_bullet = Bullet(ai_settings, screen, ship)
-        bullets.add(new_bullet)
-
+        fire_bullet(ai_settings, screen, ship, bullets)
 def check_keyup_events(event, ship):
     """Respond to keyreleases"""
     if event.key == pygame.K_RIGHT:
@@ -50,3 +47,21 @@ def update_screen(ai_settings, screen, ship, bullets):
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
+
+def update_bullets(bullets):
+    """Update position of bullets and get rid of old bullets."""
+    # Update bullet positions.
+    bullets.update()
+
+    # Get rid of bullets that have disappeared.
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+
+def fire_bullet(ai_settings, screen, ship, bullets):
+    """Fire a bullet if limit not reached yet."""
+    
+    # Create a new bullet and add it to the bullets group.
+    if len(bullets) < ai_settings.bullets_allowed:
+        new_bullet = Bullet(ai_settings, screen, ship)
+        bullets.add(new_bullet)
