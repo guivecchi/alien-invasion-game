85a3e50db165a15797623da2146b7a488922c26e
Guilherme Vecchi <guilhermebvecchi@hotmail.com>
2019-04-01 15:17:02 -0300

    Create the alien class
    Create alien class and update main file
    and game_functions to work with it

--- alien_invasion.py
+++ alien_invasion.py
@@ -3,6 +3,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 from pygame.sprite import Group
+from alien import Alien
 
 import game_functions as gf
 
@@ -21,11 +22,14 @@ def run_game():
     # Make a group to store bullets in.
     bullets = Group()
 
+    # Make an alien.
+    alien = Alien(ai_settings, screen)
+
     # Start the main loop for the game.
     while True:
         gf.check_events(ai_settings, screen, ship, bullets)
         ship.update()
         gf.update_bullets(bullets)
-        gf.update_screen(ai_settings, screen, ship, bullets)
+        gf.update_screen(ai_settings, screen, ship, alien, bullets)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -38,7 +38,7 @@ def check_keyup_events(event, ship):
         ship.moving_left = False
 
 
-def update_screen(ai_settings, screen, ship, bullets):
+def update_screen(ai_settings, screen, ship, alien, bullets):
     """Update images on the screen and flip to the new screen."""
     # Redraw the screen during each pass through the loop.
     screen.fill(ai_settings.bg_color)
@@ -47,6 +47,7 @@ def update_screen(ai_settings, screen, ship, bullets):
         bullet.draw_bullet()
 
     ship.blitme()
+    alien.blitme()
 
     # Make the most recently drawn screen visible.
     pygame.display.flip()
